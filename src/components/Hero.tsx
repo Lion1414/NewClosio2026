193940a265a48a5cae2a99979770bd7105107ca5
Lion King <86064418+Lion1414@users.noreply.github.com>
2025-12-30 20:28:56 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ const HeroDrainLines: React.FC = () => {
       </defs>
 
       <path
-        d="M 100 700 L 100 280 Q 100 210, 30 210 L -100 210"
+        d="M 80 700 L 80 420 Q 80 350, 150 350 L 350 350 Q 420 350, 420 280 L 420 -100"
         fill="none"
         stroke="rgba(106, 212, 242, 0.15)"
         strokeWidth="32"
@@ -50,7 +50,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 100 700 L 100 280 Q 100 210, 30 210 L -100 210"
+        d="M 80 700 L 80 420 Q 80 350, 150 350 L 350 350 Q 420 350, 420 280 L 420 -100"
         fill="none"
         stroke="#6ad4f2"
         strokeWidth="16"
@@ -60,7 +60,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 280 700 L 280 150 Q 280 80, 350 80 L 1700 80"
+        d="M 200 700 L 200 520 Q 200 450, 130 450 L -100 450"
         fill="none"
         stroke="rgba(255, 255, 255, 0.15)"
         strokeWidth="32"
@@ -69,7 +69,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 280 700 L 280 150 Q 280 80, 350 80 L 1700 80"
+        d="M 200 700 L 200 520 Q 200 450, 130 450 L -100 450"
         fill="none"
         stroke="white"
         strokeWidth="16"
@@ -79,7 +79,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 480 700 L 480 220 Q 480 150, 410 150 L -100 150"
+        d="M 320 700 L 320 200 Q 320 130, 250 130 L -100 130"
         fill="none"
         stroke="rgba(30, 30, 30, 0.25)"
         strokeWidth="32"
@@ -88,7 +88,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 480 700 L 480 220 Q 480 150, 410 150 L -100 150"
+        d="M 320 700 L 320 200 Q 320 130, 250 130 L -100 130"
         fill="none"
         stroke="rgba(50, 50, 50, 0.95)"
         strokeWidth="16"
@@ -98,7 +98,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 700 700 L 700 100 Q 700 30, 630 30 L -100 30"
+        d="M 480 700 L 480 300 Q 480 230, 550 230 L 680 230 Q 750 230, 750 160 L 750 -100"
         fill="none"
         stroke="rgba(213, 147, 192, 0.15)"
         strokeWidth="32"
@@ -107,7 +107,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 700 700 L 700 100 Q 700 30, 630 30 L -100 30"
+        d="M 480 700 L 480 300 Q 480 230, 550 230 L 680 230 Q 750 230, 750 160 L 750 -100"
         fill="none"
         stroke="#d593c0"
         strokeWidth="16"
@@ -117,7 +117,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 920 700 L 920 180 Q 920 110, 990 110 L 1700 110"
+        d="M 600 700 L 600 80 Q 600 10, 530 10 L -100 10"
         fill="none"
         stroke="rgba(255, 255, 255, 0.15)"
         strokeWidth="32"
@@ -126,7 +126,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 920 700 L 920 180 Q 920 110, 990 110 L 1700 110"
+        d="M 600 700 L 600 80 Q 600 10, 530 10 L -100 10"
         fill="none"
         stroke="white"
         strokeWidth="16"
@@ -136,7 +136,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 1150 700 L 1150 260 Q 1150 190, 1220 190 L 1700 190"
+        d="M 1450 700 L 1450 400 Q 1450 330, 1520 330 L 1700 330"
         fill="none"
         stroke="rgba(30, 30, 30, 0.25)"
         strokeWidth="32"
@@ -145,7 +145,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 1150 700 L 1150 260 Q 1150 190, 1220 190 L 1700 190"
+        d="M 1450 700 L 1450 400 Q 1450 330, 1520 330 L 1700 330"
         fill="none"
         stroke="rgba(50, 50, 50, 0.95)"
         strokeWidth="16"
@@ -155,7 +155,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 1380 700 L 1380 320 Q 1380 250, 1450 250 L 1700 250"
+        d="M 1550 700 L 1550 200 Q 1550 130, 1480 130 L 1350 130 Q 1280 130, 1280 60 L 1280 -100"
         fill="none"
         stroke="rgba(106, 212, 242, 0.15)"
         strokeWidth="32"
@@ -164,7 +164,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 1380 700 L 1380 320 Q 1380 250, 1450 250 L 1700 250"
+        d="M 1550 700 L 1550 200 Q 1550 130, 1480 130 L 1350 130 Q 1280 130, 1280 60 L 1280 -100"
         fill="none"
         stroke="#6ad4f2"
         strokeWidth="16"
@@ -174,7 +174,7 @@ const HeroDrainLines: React.FC = () => {
       />
 
       <path
-        d="M 1520 700 L 1520 140 Q 1520 70, 1450 70 L -100 70"
+        d="M 1380 700 L 1380 520 Q 1380 450, 1450 450 L 1700 450"
         fill="none"
         stroke="rgba(213, 147, 192, 0.15)"
         strokeWidth="32"
@@ -183,7 +183,7 @@ const HeroDrainLines: React.FC = () => {
         className="blur-md"
       />
       <path
-        d="M 1520 700 L 1520 140 Q 1520 70, 1450 70 L -100 70"
+        d="M 1380 700 L 1380 520 Q 1380 450, 1450 450 L 1700 450"
         fill="none"
         stroke="#d593c0"
         strokeWidth="16"
